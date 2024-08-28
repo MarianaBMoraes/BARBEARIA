dfe9f74cd1ef3d062e4ff3a1aaebe03f6d28bffb
Mariana <marianabmoraes.ef@gmail.com>
2024-08-28 08:55:47 -0300

    feat: index completo

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 const atendimento_controller = require("./controllers/atendimento.js");
 const servico_controller = require("./controllers/servico.js");
-const servico_controller = require("./controllers/servico.js");
+const cliente_controller = require("./controllers/cliente.js");
+const barbeiro_controller = require("./controllers/barbeiro.js");
+const barbearia_controller = require("./controllers/barbearia.js");
+const rede_controller = require("./controllers/rede.js");
+const usuario_controller = require("./controllers/usuario.js");
 const app = express();
 const port = 3000;
 
@@ -154,35 +158,64 @@ app.delete("/barbearia/:id", (req, res) => {
 
 // GERENCIAMENTO DE BARBEARIAS
 
-// GERENCIAMENTO DE BARBEARIAS
-app.post("/barbearia", (req, res) => {
-  const barbearia = req.body;
-  const code = barbearia_controller.store(barbearia);
+// GERENCIAMENTO DE REDES
+app.post("/rede", (req, res) => {
+  const rede = req.body;
+  const code = rede_controller.store(rede);
   res.status(code).json();
 });
 
-app.get("/barbearia", (req, res) => {
-  const barbearias = barbearia_controller.index();
-  res.json(barbearias);
+app.get("/rede", (req, res) => {
+  const redes = rede_controller.index();
+  res.json(redes);
 });
 
-app.get("/barbearia/:id", (req, res) => {
-  const barbearia = barbearia_controller.show(req.params.id);
-  res.json(barbearia);
+app.get("/rede/:id", (req, res) => {
+  const rede = rede_controller.show(req.params.id);
+  res.json(rede);
 });
 
-app.put("/barbearia/:id", (req, res) => {
-  const barbearia = req.body;
-  const code = barbearia_controller.update(req.params.id, barbearia);
+app.put("/rede/:id", (req, res) => {
+  const rede = req.body;
+  const code = rede_controller.update(req.params.id, rede);
   res.status(code).json();
 });
 
-app.delete("/barbearia/:id", (req, res) => {
-  barbearia_controller.destroy(req.params.id);
+app.delete("/rede/:id", (req, res) => {
+  rede_controller.destroy(req.params.id);
   res.json();
 });
 
-// GERENCIAMENTO DE BARBEARIAS
+// GERENCIAMENTO DE USUARIOS
+
+app.post("/usuario", (req, res) => {
+  const usuario = req.body;
+  const code = usuario_controller.store(usuario);
+  res.status(code).json();
+});
+
+app.get("/usuario", (req, res) => {
+  const usuarios = usuario_controller.index();
+  res.json(usuarios);
+});
+
+app.get("/usuario/:id", (req, res) => {
+  const usuario = usuario_controller.show(req.params.id);
+  res.json(usuario);
+});
+
+app.put("/usuario/:id", (req, res) => {
+  const usuario = req.body;
+  const code = usuario_controller.update(req.params.id, usuario);
+  res.status(code).json();
+});
+
+app.delete("/usuario/:id", (req, res) => {
+  usuario_controller.destroy(req.params.id);
+  res.json();
+});
+
+// GERENCIAMENTO DE USUARIOS
 
 
 app.listen(port, () => {
